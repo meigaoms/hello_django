0317b03eb72fef6d6af354b35314a116284db9db
Mei Gao <xuga@microsoft.com>
2020-04-13 10:24:40 -0700

    Change url to re_path

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
 
@@ -25,5 +25,5 @@ urlpatterns = [
     path('insta/', include('Insta.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('auth/signup/', SignUp.as_view(), name="sign_up"),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
