63e7c871f9f33060a1d00e9ca7900c4621bb2795
Mei Gao <xuga@microsoft.com>
2020-04-12 23:14:49 -0700

    Change allow_hosts to *

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!n!j@t$m)wj+ubqsee-w&f)*&=p&lw!p-mk&2e4rc%l(-isz4s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
